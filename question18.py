85d1750dea648302168acbf7cfe1c893caadc215
Butthacha Arom <63070097@kmitl.ac.th>
2020-12-04 20:21:50 +0700

    ques14-19

--- question18.py
+++ question18.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 def question18():  
     def sel():
         """1 ans"""
@@ -18,27 +19,38 @@ def question18():
     root.title("question")#ชื่อ
     root.geometry("600x500")
     root.option_add("*font","Opun-Mai-Thin 10 bold")
-    frame = Frame(root)
-    frame.config(background="#C6E0E0")
-    frame.place(width=600, height=500, x=0, y=0)
-    root.option_add("*foreground", "navy")
-    label1 = Label(frame, text="QUESTION ?", width=15, height = 2, bg="white")
-    label1.grid(column=0,row=0, columnspan=2,padx=15,pady=30)
-    label2 = Label(frame, text= "ประเภทของข้อมูลใดที่เราควรใช้\nเพื่อจัดเก็บข้อมูลของจำนวนนักศึกษาในคลาสเรียน" , width=50, height = 5, bg="white")
-    label2.grid(column=0, row=1, columnspan=3, padx=10,pady=3)
+
+    canvas = Canvas(root, width=600, height=500)
+    canvas.pack()
+
+    photo = ImageTk.PhotoImage(Image.open('Q2.png'))
+    canvas.create_image(300, 250, image=photo)
+    # frame = Frame(root)
+    # frame.config(background="#C6E0E0")
+    # frame.place(width=600, height=500, x=0, y=0)
+    # root.option_add("*foreground", "navy")
+    # label1 = Label(frame, text="QUESTION ?", width=15, height = 2, bg="white")
+    # label1.grid(column=0,row=0, columnspan=2,padx=15,pady=30)
+    label2 = Label(root, text= "ประเภทของข้อมูลใดที่เราควรใช้\nเพื่อจัดเก็บข้อมูลของจำนวนนักศึกษาในคลาสเรียน" , width=50, height = 5, bg="white")
+    canvas.create_window(300, 105, anchor='center', window=label2)
+    # label2.grid(column=0, row=1, columnspan=3, padx=10,pady=3)
     var = IntVar()
-    r1 = Radiobutton(frame, text="int", width=15, height=3, variable=var, value=1,
+    r1 = Radiobutton(root, text="int", width=15, height=3, variable=var, value=1,
                     bg="white", activebackground='green', command=sel)#คำตอบแรก
-    r1.grid(column=0, row=2, padx=75, pady=30)
-    r2 = Radiobutton(frame, text="bool", width=15, height=3 ,variable=var, value=2,
+    canvas.create_window(300, 190, anchor='center', window=r1)
+    # r1.grid(column=0, row=2, padx=75, pady=30)
+    r2 = Radiobutton(root, text="bool", width=15, height=3 ,variable=var, value=2,
                     bg="white", activebackground='red', command=sel)  # คำตอบสอง
-    r2.grid(column=1, row=2, padx=75, pady=15)
-    r3 = Radiobutton(frame, text="float", width=15, height=3, variable=var, value=3,
+    canvas.create_window(300, 260, anchor='center', window=r2)
+    # r2.grid(column=1, row=2, padx=75, pady=15)
+    r3 = Radiobutton(root, text="float", width=15, height=3, variable=var, value=3,
                     bg="white", activebackground='red', command=sel)  # คำตอบสาม
-    r3.grid(column=0, row=3, padx=15, pady=15)
-    r4 = Radiobutton(frame, text="string", width=15, height=3, variable=var, value=4,
+    canvas.create_window(300, 330, anchor='center', window=r3)
+    # r3.grid(column=0, row=3, padx=15, pady=15)
+    r4 = Radiobutton(root, text="string", width=15, height=3, variable=var, value=4,
                     bg="white", activebackground='red', command=sel)  # คำตอบสี่
-    r4.grid(column=1, row=3, padx=15, pady=15)
+    canvas.create_window(300, 400, anchor='center', window=r4)
+    # r4.grid(column=1, row=3, padx=15, pady=15)
     label = Label(root)
     label.pack()
     root.mainloop()
