895e1efb80f28ae03c98cfd724cdc26476657f9c
_porrr_ <63070188@kmitl.ac.th>
2020-12-08 11:49:52 +0700

    edite

--- question_12.py
+++ question_12.py
@@ -38,22 +38,22 @@ def question12():
     # label2.grid(column=0, row=1, padx=80, pady=0.1)
     var = IntVar()
     r1 = Radiobutton(root, text="True and True or False and True       ",  width=30, height=3, variable=var, value=1,
-                    bg="white", activebackground='green', command=sel)
+                    bg="white", activebackground='red', command=sel)
     canvas.create_window(300, 190, anchor='center', window=r1)
     # c1.grid(column=0, row=2, padx=80, pady=10)
     # var2 = IntVar()
     r2 = Radiobutton(root, text="False and True or False and True      ",  width=30, height=3, variable=var, value=2,
-                    bg="white", activebackground='red', command=sel)
+                    bg="white", activebackground='green', command=sel)
     canvas.create_window(300, 260, anchor='center', window=r2)
     # c2.grid(column=0, row=3, padx=80, pady=10)
     # var3 = IntVar()
     r3 = Radiobutton(root, text="True or (8-5 == 3) and True or False",  width=30, height=3, variable=var, value=3,
-                    bg="white", activebackground='green', command=sel)
+                    bg="white", activebackground='red', command=sel)
     canvas.create_window(300, 330, anchor='center', window=r3)
     # c3.grid(column=0, row=4, padx=80, pady=10)
     # var4 = IntVar()
     r4 = Radiobutton(root, text="(2 >= 0) or (-5 <= -8)                         ",  width=30, height=3, variable=var, value=4,
-                    bg="white", activebackground='green', command=sel)
+                    bg="white", activebackground='red', command=sel)
     canvas.create_window(300, 400, anchor='center', window=r4)
     # c4.grid(column=0, row=5, padx=80, pady=10)
     label = Label(root)
