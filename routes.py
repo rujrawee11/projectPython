e1a423beb428d3915b851cf5f1287329de4e8cb0
Rujrawee Meela <63070151@it.kmitl.ac.th>
2020-12-12 14:29:27 +0700

    add detail

--- routes.py
+++ routes.py
@@ -6,10 +6,10 @@ class WindowRoute:
     def __init__(self, master):
         self.master = master
 
-    # สร้างหน้าต่างเกม เป่า ยิ้ง ฉุบ
+    # สร้างหน้าต่างเกม เป่า ยิง ฉุบ
     def startGame(self):
-        self.master.lower() # ยุบหน้าจอ main menu ลง (master นะที่นี้คือ หน้าต่าง main menu)
-        self.master.attributes('-topmost', 0) # ปรับลำดับหน้าจอ main menu ลงให้ต่ำสุด หรืออยู่ข้างล่างนั้นแหละ
+        self.master.lower() # ยุบหน้าจอ main menu ลง (master คือ หน้าต่าง main menu)
+        self.master.attributes('-topmost', 0) # ปรับลำดับหน้าจอ main menu ลงให้ต่ำสุด หรืออยู่ข้างล่าง
         self.newWindow = Toplevel(self.master) # สร้างหน้าใหม่ขึ้นมาซ้อนไว้
         self.newWindow.protocol("WM_DELETE_WINDOW", self.on_backing) # เมื่อมีการปิดหน้าต่างให้ เรียกฟังก์ชัน on_backing ของ class นี้
         game = GameController(self.newWindow) # สร้าง object จาก class GameController ขึ้นมา
@@ -17,19 +17,19 @@ class WindowRoute:
     
     # สร้างหน้าต่างวิธีเล่น
     def instruction(self):
-        self.master.lower() # ยุบหน้าจอ main menu ลง (master นะที่นี้คือ หน้าต่าง main menu)
-        self.master.attributes('-topmost', 0) # ปรับลำดับหน้าจอ main menu ลงให้ต่ำสุด หรืออยู่ข้างล่างนั้นแหละ
+        self.master.lower()  # ยุบหน้าจอ main menu ลง (master คือ หน้าต่าง main menu)
+        self.master.attributes('-topmost', 0) # ปรับลำดับหน้าจอ main menu ลงให้ต่ำสุด หรืออยู่ข้างล่าง
         self.newWindow = Toplevel(self.master) # สร้างหน้าใหม่ขึ้นมาซ้อนไว้
         self.newWindow.protocol("WM_DELETE_WINDOW", self.on_backing)  # เมื่อมีการปิดหน้าต่างให้ เรียกฟังก์ชัน on_backing ของ class นี้
         instructionWindow(self.newWindow) # เรียกฟังก์ชัน instructionWindow เพิ่มแสดงผลหน้าต่างวิธีการเล่น
 
     # event สำหรับกลับไปจอ main menu
     def on_backing(self):
-        self.master.lift() # ขยายหรือนำหน้าจอ main menu กลับขึ้นมา (master นะที่นี้คือ หน้าต่าง main menu)
+        self.master.lift()  # ขยายหรือนำหน้าจอ main menu กลับขึ้นมา (master คือ หน้าต่าง main menu)
         self.master.attributes('-topmost', 1) # ปรับลำดับหน้าจอ main menu ลงให้ขึ้นมาบนสุด
         self.newWindow.destroy() # ทำลายหรือปืดหน้าต่างที่เปิดอยู่ เช่น หน้าต่างเกม หน้าวิธีเล่น
 
     # event สร้าง dialog ให้ยืนยันก่อนปิดหน้าต่าง
     def on_closing(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"): # สร้าง dialog ให้ยืนยันก่อนปิดหน้าต่าง
-            self.master.destroy() # ทำลาย mian menu เพื่อปิดเกม
+            self.master.destroy() # ทำลาย main menu เพื่อปิดเกม
